a8613d7180adebb71728e9d8a85ec583e6849c99
Md. Nazmul Siddique <27219@wgbd.com>
2025-09-14 19:00:46 +0600

    pound cake page done

--- pound-cake.php
+++ pound-cake.php
@@ -15,7 +15,7 @@
         <header class="main-menu">
             <nav class="navbar navbar-expand-lg">
                 <div class="container">
-                    <a class="navbar-brand" href="index.php">
+                    <a class="navbar-brand" href="#">
                         <img src="images/logo.png" alt="Chef Recipe">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -25,7 +25,7 @@
                     </button>
                     <div class="collapse navbar-collapse" id="navbarScroll">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item px-4"><a class="nav-link px-0" href="index.php">Home</a></li>
+                            <li class="nav-item px-4"><a class="nav-link px-0" href="#">Home</a></li>
                             <li class="nav-item px-4"><a class="nav-link px-0" href="#">Products</a></li>
                             <li class="nav-item px-4"><a class="nav-link px-0" href="#">About Us</a></li>
                             <li class="nav-item px-4"><a class="nav-link px-0" href="#">How To & Tips</a></li>
@@ -37,16 +37,6 @@
                                 <option value="1">বাংলা</option>
                             </select>
                         </div>
-                        <!-- <div class="dropdown">
-                            <button class="btn dropdown-toggle fw-semibold" type="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                English
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a class="dropdown-item" href="#">English</a></li>
-                                <li><a class="dropdown-item" href="#">বাংলা</a></li>
-                            </ul>
-                        </div> -->
                     </div>
                 </div>
             </nav>
@@ -55,22 +45,19 @@
         <section class="pb-5 pb-lg-0 mt-5">
             <div class="container">
                 <div class="row align-items-start position-relative" style="min-height: 600px;">
-                    <div class="col-lg-7">
-                        <h1 class="hero-title">Chef’s Perfect Recipe</h1>
+                    <div class="col-lg-6">
+                        <h1 class="hero-title">Pound Cake</h1>
                         <p class="pb-5 pe-md-5 hero-description">
-                            With Chef’s Perfect Recipe, cooking becomes easier and more reliable.
-                            It adjusts the right ingredients, time, and settings for your oven to give you
-                            perfect results every time. Whether you’re baking a cake or making a pizza,
-                            our recipe calculator makes cooking simpler, faster, and tastier.
+                            Our pound cake is made with the perfect blend of ingredients, delivering a soft and moist texture with a rich, classic taste. Whether it’s for an afternoon snack, a special celebration, or simply a sweet treat every slice will bring you joy and satisfaction.
                         </p>
                         <div class="d-md-none cursor-pointer">
-                            <img class="img-fluid" src="images/top-view-delicious-pizza.png">
+                            <img class="img-fluid" src="images/pound-cake-lerge.png">
                         </div>
                     </div>
 
                     <div>
                         <div class="text-center text-lg-end position-absolute left-0 top-0 d-none d-md-block cursor-pointer" style="width: 700px; right:0;">
-                            <img class="img-fluid mt-5" src="images/top-view-delicious-pizza.png">
+                            <img class="img-fluid mt-5" src="images/pound-cake-lerge.png">
                         </div>
                     </div>
                 </div>
@@ -79,84 +66,135 @@
 
     </div>
 
-    <div class="category-area mt-5 mb-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 mt-4">
-                    <a href="pound-cake.php">
-                    <div class="category-item">
-                        <img src="images/pound-cake.png" alt="Category 1" class="img-fluid py-3 px-2">
-                        <div class="category-info">
-                            <h5 class="mt-3">POUND CAKE</h5>
-                            <p>Rich and moist pound cake, baked to perfection for a classic taste in every bite.</p>
+    <div class="filter-area mt-5 mb-5">
+        <div class="container pound-cake">
+            <h2 class="pound-cake-title mb-5"> <span></span> Easily calculate your recipe in seconds</h2>
+            <form action="">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="mb-3">
+                            <label for="cake_weight" class="form-label">How many grams of cake do you want to make?</label>
+                            <input type="text" class="form-control" id="cake_weight" placeholder="Enter your cake weight" required>
                         </div>
                     </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 mt-4">
-                    <div class="category-item">
-                        <img src="images/sponge-cake.png" alt="Category 2" class="img-fluid py-3 px-2">
-                        <div class="category-info">
-                            <h5 class="mt-3">SPONGE CAKE <span class="upcoming-text">(Upcoming)</span></h5>
-                            <p>Soft and fluffy sponge cake with a light texture, perfect for tea-time or dessert.</p>
+                    <div class="col-lg-4">
+                        <div class="mb-3">
+                            <label for="oven_model" class="form-label">Which model of oven do you want to use for baking the cake?</label>
+                            <select class="form-select w-100" id="oven_model" required>
+                                <option value="" selected disabled hidden>Select your model</option>
+                                <option value="1">M32CTS</option>
+                                <option value="2">M25CDS</option>
+                                <option value="3">M30AS3</option>
+                                <option value="4">G30SCT</option>
+                                <option value="5">25CDP</option>
+                            </select>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-3 mt-4">
-                    <div class="category-item">
-                        <img src="images/Dry Cake.png" alt="Category 3" class="img-fluid py-3 px-2">
-                        <div class="category-info">
-                            <h5 class="mt-3">Dry Cake <span class="upcoming-text">(Upcoming)</span></h5>
-                            <p>Crisp and golden dry cake, a delightful snack for any time of the day.</p>
-                        </div>
+                    <div class="col-lg-4">
+                        <button type="submit" class="btn btn-primary w-100">Submit</button>
                     </div>
                 </div>
-                <div class="col-lg-3 mt-4">
-                    <div class="category-item">
-                        <img src="images/Bun.png" alt="Category 3" class="img-fluid py-3 px-2">
-                        <div class="category-info">
-                            <h5 class="mt-3">Bun <span class="upcoming-text">(Upcoming)</span></h5>
-                            <p>Freshly baked soft buns, ideal for burgers, sandwiches, or a simple snack.</p>
+                <div class="row mt-5">
+                    <div class="col-lg-8">
+                        <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Ingredients</th>
+                                <th>Quantity</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Flour</td>
+                                <td>140 g</td>
+                            </tr>
+                            <tr>
+                                <td>Sugar</td>
+                                <td>140 g</td>
+                            </tr>
+                            <tr>
+                                <td>Egg</td>
+                                <td>140 g</td>
+                            </tr>
+                            <tr>
+                                <td>Powder Milk</td>
+                                <td>3 g</td>
+                            </tr>
+                            <tr>
+                                <td>Soybean Oil</td>
+                                <td>71 g</td>
+                            </tr>
+                            <tr>
+                                <td>Baking Powder</td>
+                                <td>6 g</td>
+                            </tr>
+                            <tr>
+                                <td>Vanilla Essence</td>
+                                <td>5 g</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td class="fw-bold">Total</td>
+                                <td class="fw-bold">500 g</td>
+                            </tr>
+                        </tfoot>
+                        </table>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="oven-model">
+                        <div class="d-flex justify-content-center mt-3 temperature">
+                            <h2>170 °</h2>
+                            <img src="images/fire-icon.png" alt="" class="img-fluid">
+                        </div>
+                        <h4>Set Temperature</h4>
+                        <p>“The temperature may need to be increased or decreased depending on the voltage.”</p>
+                        <img src="images/oven-1.png" alt="" class="img-fluid">
                         </div>
                     </div>
+                    
                 </div>
-                <div class="col-lg-3 mt-4">
-                    <div class="category-item">
-                        <img src="images/Tandoori Chicken.png" alt="Category 3" class="img-fluid py-3 px-2">
-                        <div class="category-info">
-                            <h5 class="mt-3">Tandoori Chicken <span class="upcoming-text">(Upcoming)</span></h5>
-                            <p>Juicy and flavorful chicken marinated with spices, roasted to a smoky perfection.</p>
+                <div class="details-info mt-5">
+                    <div class="row">
+                        <div class="col-lg-12">
+                           <h2 class="pound-cake-title mb-5"> <span></span>  Details Recipe</h2>
                         </div>
+                        <div class="col-lg-7">
+                            <p><strong>Step 1:</strong> Sift together flour, milk powder, and baking powder into a bowl and mix well.</p>
+                            <p><strong>Step 2:</strong> In another bowl, beat the eggs with a beater. Gradually add sugar while beating until it becomes fluffy and foamy.</p>
+                            <p><strong>Step 3:</strong> Gradually add the flour mixture into the egg mixture and mix well. Then add soybean oil and vanilla essence (drops) and combine thoroughly.</p>
+                            <p><strong>Step 4:</strong> Before pouring the batter, line the mold with baking paper and brush lightly with oil. Then pour the batter into the mold.</p>
+                        </div>
+                        <div class="col-lg-5">
+                            <img class="img-fluid" src="images/details-image-one.jpg" alt="" >
+                        </div>
+
+                        <div class="col-lg-5 mt-3">
+                            <img class="img-fluid" src="images/details-image-two.png" alt="" >
+                        </div>
+                        <div class="col-lg-7 mt-3">
+                            <p><strong>Step 1:</strong> Preheat the oven in convection mode at the set temperature for 5 minutes. Once preheating is complete, place the mold with the batter into the oven.</p>
+                            <p><strong>Step 2:</strong> Set the convection time for 40–60 minutes, adjust the temperature as required, and start the oven.</p>
+                            <p><strong>Step 3:</strong> Baking time may vary depending on voltage, mold, and type of cake. After about 30-40 minutes, insert a dry stick into the cake to check. If the stick comes out with batter, the cake is not ready yet. If it comes out clean, the cake is done.</p>
+                        </div>
+
+                        <div class="col-lg-12 mt-5">
+                            <h2 class="pound-cake-title mb-5"> <span></span> Cake Baking Tips</h2>
+                            <p>1. If baking powder is old, the cake will collapse, so always use good quality and fresh baking powder.</p>
+                            <p>2. Never store baking powder in the refrigerator; always keep it at room temperature.</p>
+                            <p>3. Do not open the oven door repeatedly while baking.</p>
+                            <p>4. Measure all ingredients accurately, as incorrect proportions may cause your cake to turn out poorly.</p>
+                            <p>5. Use eggs at normal room temperature. Take them out of the refrigerator at least 30 minutes before use.</p>
+                        </div>
+
+                        <div class="col-lg-12 mt-5">
+                            <h2 class="pound-cake-title mb-5"> <span></span> Note</h2>
+                            <p>Each recipe is tested in our lab. However, variations in ingredients and cooking methods may affect the quality of the food.</p>
+                        </div>
+                        
                     </div>
                 </div>
-                <div class="col-lg-3 mt-4">
-                    <div class="category-item">
-                        <img src="images/Pizza.png" alt="Category 3" class="img-fluid py-3 px-2">
-                        <div class="category-info">
-                            <h5 class="mt-3">Pizza <span class="upcoming-text">(Upcoming)</span></h5>
-                            <p>A pizza made with fresh ingredients, offering an authentic Italian taste in every slice.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mt-4">
-                    <div class="category-item">
-                        <img src="images/Maltova Biscuit.png" alt="Category 3" class="img-fluid py-3 px-2">
-                        <div class="category-info">
-                            <h5 class="mt-3">Maltova Biscuit <span class="upcoming-text">(Upcoming)</span></h5>
-                            <p>Golden and crumbly biscuits, baked to give a rich taste in every bite.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mt-4">
-                    <div class="category-item">
-                        <img src="images/Butter salt Biscuit.png" alt="Category 3" class="img-fluid py-3 px-2">
-                        <div class="category-info">
-                            <h5 class="mt-3">Butter salt Biscuit <span class="upcoming-text">(Upcoming)</span></h5>
-                            <p>Crispy, light, and perfectly round biscuits for a quick and tasty snack.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </form>
         </div>
     </div>
 
